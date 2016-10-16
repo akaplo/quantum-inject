de0d42c673949d8516d39207d50ef1bab31d3ffb
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-10-16 13:02:39 -0400

    Added some preliminary scapy tcp sniffing and regex matching

--- quantum.py
+++ quantum.py
@@ -1,5 +1,8 @@
+# python 2.7
 # how to run: python quantum.py --interface eth0 --regexp /^regex$/ --datafile someFIle expr
 import argparse
+import scapy
+import re
 
 parser = argparse.ArgumentParser(description='idk')
 parser.add_argument("--interface", help="interface")
@@ -13,3 +16,15 @@ print args.interface
 print args.regexp
 print args.datafile
 print args.expression
+
+# sniff the given interface for tcp packets
+packets = scapy.sniff(iface=args.interface, filter="tcp")
+# compile the given regex for use
+regex = re.compile(args.regexp)
+# must check each packet individually
+for packet in  packets:
+    # re.match vs re.search?
+    # packet must be a string, so this may not work
+    # See if the packet matches the given regex
+    if(re.search(regex, packet)):
+        print packet
