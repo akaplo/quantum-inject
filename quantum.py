7c946d43d59b0446a017b85cd05cf45f2068cade
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-10-15 22:13:28 -0400

    Put all the arguments in

--- quantum.py
+++ quantum.py
@@ -1,3 +1,15 @@
-import sys
+# how to run: python quantum.py --interface eth0 --regexp /^regex$/ --datafile someFIle expr
+import argparse
 
-print sys.argv
+parser = argparse.ArgumentParser(description='idk')
+parser.add_argument("--interface", help="interface")
+parser.add_argument("--regexp", help="regular expression")
+parser.add_argument("--datafile", help="datafile input")
+parser.add_argument("expression", help="straight up input")
+
+args = parser.parse_args()
+print args
+print args.interface
+print args.regexp
+print args.datafile
+print args.expression
