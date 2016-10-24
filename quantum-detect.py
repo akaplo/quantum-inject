ffee07a5688dfd24398ddd1bf4497161a5318ca3
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-10-24 15:18:24 -0400

    Overhauled detector and it seems to be working

--- quantum-detect.py
+++ quantum-detect.py
@@ -24,53 +24,51 @@ args = parser.parse_args()
 
 sniffed = []
 
-def find_injected_packets(pkt_list):
-    #print 'hello'
-    for index, packet in enumerate(pkt_list):
-        #print index
-        #print sniffed_packets
-        try:
-            packet[TCP][Raw]
-        except IndexError:
-            print 'packet doesnt have raw payload'
-        else:
-            try:
-                pkt_list[index+1][TCP][Raw]
-            except IndexError:
-                print 'packet doesnt have raw payload'
-            else:
-                if index < len(pkt_list) - 1:
-                    #print packet[TCP].seq
-                    if packet[TCP].seq == pkt_list[index+1][TCP].seq:
-                        # the sequence numbers are equal. Might be a duplicate!
-                        # Let's check for equivalent acks too
-                        print 'matched seqnum'
-                        if packet[TCP].ack == pkt_list[index+1][TCP].ack:
-                            # the ack numbers are equal too!
-                            # If the payload is not the same, then
-                            # we can say the packet that came first is injected
-                            print 'matched acknum'
-                            #print packet[TCP][Raw].load
-                            #print pkt_list[index+1][TCP][Raw].load
-                            if packet[TCP][Raw].load != pkt_list[index+1][TCP][Raw].load:
-                                print 'Looks like an injected packet!'
-                                print packet[TCP][Raw].load
-
-
-def append_pkt(pkt):
-    print 'append packet'
-    global sniffed
-    if len(sniffed) > 500:
-        print 'got to 500 packets!'
-        sniffed.sort(key=lambda pkt: pkt[TCP].seq)
-        find_injected_packets(sniffed)
-        sniffed = sniffed[400:500]
-    else:
-        sniffed.append(pkt)
+def find_injected_packets(packet):
+    # We know the packet has the Raw layer, make sure it has a payload too
+    if hasattr(packet.getlayer('Raw'), 'load'):
+        # Go through every other sniffed packet and see if this one matches any
+        for check_against in sniffed:
+            # IP destination is same?
+            if check_against[IP].dst == packet[IP].dst:
+                # IP source is same?
+                if check_against[IP].src == packet[IP].src:
+                    # TCP source port is same?
+                    if check_against[TCP].sport == packet[TCP].sport:
+                        # IP destination port is same?
+                        if check_against[TCP].dport == packet[TCP].dport:
+                            # TCP sequence number is same?
+                            if check_against[TCP].seq == packet[TCP].seq:
+                                # TCP ack number is same?
+                                if check_against[TCP].ack == packet[TCP].ack:
+                                    # So far, we'd match any packet that's a duplicate
+                                    # or retransmision.
+                                    # If the packet was injected, all of the above would still
+                                    # hold, but the payload wouldn't match:
+                                    if check_against[TCP][Raw].load != packet[TCP][Raw].load:
+                                        print '************************************************************************'
+                                        print 'injection motherfucker'
+                                        # We can assume that the legitmate packet arrived after the
+                                        # injected one, so check_against must've been injected, since
+                                        # the nature of this function is such that a packet from the global
+                                        # list is less recent than the packet that this function was called with.
+                                        print "Injected packet's payload:"
+                                        print check_against[Raw].load
+                                        print "Legitmate packet's payload:"
+                                        print packet[Raw].load
+                                        print '************************************************************************'
+        # Now outside of the loop, we add the newest packet to the
+        # global list so that it can be used to check against newer packets
+        sniffed.append(packet)
 
 if (args.file):
-    input_dump = rdpcap(args.file)
-    sorted_dump = sorted(input_dump, key=lambda pkt: pkt[TCP].seq)
-    find_injected_packets(sorted_dump)
+    packets = sniff(offline=args.file, filter="tcp and port 80", lfilter=lambda r: r.haslayer(Raw), prn=lambda pkt: find_injected_packets(pkt))
 else:
-    sniffed_packets = sniff(iface=args.interface, filter="tcp port 80", lfilter=lambda r: r.haslayer(Raw), prn=append_pkt)
+    # Sniff packets straight from the given network interface.
+
+    # We arbitrarily limit the number of packets to capture before closing up shop
+    # for the sake of speed.  If packets are coming rapid-fire, find_injected_packets
+    # may get so far behind (sure, it's a linear function, but still...)
+    # that the script will be reporting injected packets
+    # long after the fact.
+    sniffed_packets = sniff(count=5000, iface=args.interface, filter="tcp and port 80", lfilter=lambda r: r.haslayer(Raw), prn=lambda k: find_injected_packets(k))
